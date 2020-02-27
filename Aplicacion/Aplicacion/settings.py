7628b6bcaba9754374ae426853b115f62472ced9
martaverdugo6 <47628669+martaverdugo6@users.noreply.github.com>
2020-02-27 14:25:45 +0100

    Crispy form para alta_usuario

--- Aplicacion/Aplicacion/settings.py
+++ Aplicacion/Aplicacion/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'gestionApp',
 ]
 
@@ -127,3 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ["C:/Users/marta/Desktop/TFG/App_Django/Aplicacion/Aplicacion/static/"]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
